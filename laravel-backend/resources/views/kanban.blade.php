d99ecd4675a7db6aafc90817da798b4a057ada47
Anjali Maurya <anjimaurya2207@gmail.com>
2026-07-21 20:50:04 +0530

    Add kanban route successfully

---
+++ laravel-backend/resources/views/kanban.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kanban Board</title>
+</head>
+<body style="font-family: Arial; background: #1b1b18; color: white; text-align: center; padding-top: 50px;">
+    <h1>🔥 My Kanban Board is Live! 🔥</h1>
+</body>
+</html>
--- laravel-backend/routes/web.php
+++ laravel-backend/routes/web.php
@@ -1,7 +1,9 @@
-<?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/kanban', function () {
+    return view('kanban');
 });
